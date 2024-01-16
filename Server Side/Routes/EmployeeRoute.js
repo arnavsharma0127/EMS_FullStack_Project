eb5562d9b51bd9645b5b6e140dc128c41f9a12d4
Arnav Sharma <115213616+arnavsharma0127@users.noreply.github.com>
2024-01-16 23:57:51 +0530

    Update EmployeeRoute.js

--- Server Side/Routes/EmployeeRoute.js
+++ Server Side/Routes/EmployeeRoute.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import con from "../utils/db.js";
-import jwt from "jsonwebtoken";
+import jwt from "jsonwebtoken";   
 import bcrypt from 'bcrypt'
 
 const router = express.Router()
