02ecfbe889729c197f5496b45cd6ab67dee8cf5a
Arnav Sharma <115213616+arnavsharma0127@users.noreply.github.com>
2024-01-22 19:53:21 +0530

    Update index.js

--- Server Side/index.js
+++ Server Side/index.js
@@ -6,7 +6,7 @@ import Jwt from "jsonwebtoken";
 import cookieParser from "cookie-parser";
 
 const app = express() 
-app.use(cors({
+app.use(cors({        
     origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
